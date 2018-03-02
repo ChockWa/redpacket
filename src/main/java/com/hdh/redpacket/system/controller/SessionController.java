a717de7798b25a98f7ac538223b9539ce5f8239c
admq <970255897@qq.com>
2018-03-03 00:22:14 +0800

    修改Controller请求地址

--- src/main/java/com/hdh/redpacket/system/controller/SessionController.java
+++ src/main/java/com/hdh/redpacket/system/controller/SessionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * session会话控制器
  */
 @RestController
-@RequestMapping("/api.do")
+@RequestMapping("/sys")
 public class SessionController {
 
     @Autowired
--- src/main/java/com/hdh/redpacket/user/controller/UserController.java
+++ src/main/java/com/hdh/redpacket/user/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/api.do")
+@RequestMapping("/user")
 @MustLogin(true)
 public class UserController {
 
