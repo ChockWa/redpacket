6a32065d71002f80a6d80c51189ab9ebc6a80dcc
hezhuohua <hzh081215@qq.com>
2018-03-23 15:22:51 +0800

    添加分页配置

--- src/main/java/com/hdh/redpacket/core/config/MybatisConfig.java
+++ src/main/java/com/hdh/redpacket/core/config/MybatisConfig.java
@@ -1,7 +1,11 @@
 package com.hdh.redpacket.core.config;
 
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 /**
  * @author duhongg
@@ -11,4 +15,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.hdh.redpacket.*.mapper*")
 public class MybatisConfig {
+
+    /**
+     * 分页配置
+     * @return
+     */
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
 }
